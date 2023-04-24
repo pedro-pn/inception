a572779099d04811cef6cdbe89ded45d4a1d9d2b
Pedro Paulo <pe.pn03@gmail.com>
2023-04-24 19:22:24 -0300

    Add second user (normal)

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -5,6 +5,8 @@ mysql_install_db
 /etc/init.d/mysql start
 mysql -u root -p -e "CREATE DATABASE wordpressdb;
 CREATE USER 'ppaulo-d'@'%' IDENTIFIED BY 'teste';
+CREATE USER 'user42'@'%' IDENTIFIED BY '123';
+GRANT SELECT, INSERT, UPDATE, DELETE ON wordpressdb.* TO 'user42'@'%' IDENTIFIED BY '123';
 GRANT ALL PRIVILEGES ON wordpressdb.* TO 'ppaulo-d'@'%' IDENTIFIED BY 'teste' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EXIT"
