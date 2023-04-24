b76909a0c3b75bfa54693867b8f1fd7b38dcb419
Pedro Paulo <pe.pn03@gmail.com>
2023-04-24 13:53:45 -0300

    Change to debian 'buster'

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
+
 mysql_install_db
 
-/etc/init.d/mariadb start
-ufw allow from localhost to any port 3306
+/etc/init.d/mysql start
 mysql -u root -p -e "CREATE DATABASE wordpressdb;
 CREATE USER 'ppaulo-d'@'%' IDENTIFIED BY 'teste';
 GRANT ALL PRIVILEGES ON wordpressdb.* TO 'ppaulo-d'@'%' IDENTIFIED BY 'teste' WITH GRANT OPTION;
@@ -9,4 +10,4 @@ FLUSH PRIVILEGES;
 EXIT"
 mysql -uppaulo-d -pteste wordpressdb < /db.sql
 
-/etc/init.d/mariadb stop
+/etc/init.d/mysql stop
