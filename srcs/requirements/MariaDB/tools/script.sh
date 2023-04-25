1885d54a8f8bfe2b85c13b1eef71abf4e5b34d45
Pedro Paulo <pe.pn03@gmail.com>
2023-04-24 21:32:22 -0300

    Add if/else guard in bash script and make it an entrypoint

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-mysql_install_db
+if [ ! -d /var/lib/mysql/wordpressdb ]; then
+	mysql_install_db
+	/etc/init.d/mysql start
+	mysql -u root -e "CREATE DATABASE wordpressdb;
+	CREATE USER 'ppaulo-d'@'%' IDENTIFIED BY 'teste';
+	CREATE USER 'user42'@'%' IDENTIFIED BY '123';
+	GRANT SELECT, INSERT, UPDATE, DELETE ON wordpressdb.* TO 'user42'@'%' IDENTIFIED BY '123';
+	GRANT ALL PRIVILEGES ON wordpressdb.* TO 'ppaulo-d'@'%' IDENTIFIED BY 'teste' WITH GRANT OPTION;
+	FLUSH PRIVILEGES;
+	EXIT"
+	mysql -uppaulo-d -pteste wordpressdb < /db.sql
+fi
 
-/etc/init.d/mysql start
-mysql -u root -p -e "CREATE DATABASE wordpressdb;
-CREATE USER 'ppaulo-d'@'%' IDENTIFIED BY 'teste';
-CREATE USER 'user42'@'%' IDENTIFIED BY '123';
-GRANT SELECT, INSERT, UPDATE, DELETE ON wordpressdb.* TO 'user42'@'%' IDENTIFIED BY '123';
-GRANT ALL PRIVILEGES ON wordpressdb.* TO 'ppaulo-d'@'%' IDENTIFIED BY 'teste' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-EXIT"
-mysql -uppaulo-d -pteste wordpressdb < /db.sql
-
-/etc/init.d/mysql stop
+exec "$@"
