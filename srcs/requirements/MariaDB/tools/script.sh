b017bdcf3ee1b74e2828ea90e0b47c5b47052741
Pedro Paulo <pe.pn03@gmail.com>
2023-04-27 12:17:46 -0300

    Add variables to redis config and secure mysql configs

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -xe
 
+
 if [ ! -d /var/lib/mysql/wordpressdb ]; then
 	mysql_install_db
 	/etc/init.d/mysql start
+
 	mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};
+	UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User='root';
+	DELETE FROM mysql.user WHERE User='';
+	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+	DROP DATABASE IF EXISTS test;
+	DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 	CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 	CREATE USER '${MYSQL_USER2}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD2}';
 	GRANT SELECT, INSERT, UPDATE, DELETE ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER2}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD2}';
--- srcs/requirements/wordpress/tools/wordpress-entrypoint.sh
+++ srcs/requirements/wordpress/tools/wordpress-entrypoint.sh
@@ -20,14 +20,14 @@ if ! cat $config_file | grep -q "Redis configuration" ; then
 	sed -i '39i\'$'\n' $config_file
 	sed -i '39i\'$'\n' $config_file
 	sed -i '40i /** Redis configuration **/' $config_file
-	sed -i "41i define( 'WP_REDIS_HOST', 'redis' );" $config_file
-	sed -i "42i define( 'WP_REDIS_PORT', '6379' );" $config_file
+	sed -i "41i define( 'WP_REDIS_HOST', '${REDIS_HOST}' );" $config_file
+	sed -i "42i define( 'WP_REDIS_PORT', '${REDIS_PORT}' );" $config_file
 	sed -i "43i define( 'WP_REDIS_PREFIX', 'inception' );" $config_file
 	sed -i "43i define( 'WP_REDIS_DATABASE', 1 );" $config_file
 	sed -i "43i define( 'WP_REDIS_TIMEOUT', 1 );" $config_file
 	sed -i "43i define( 'WP_REDIS_READ_TIMEOUT', 1 );" $config_file
 	sed -i "68i define('WP_CACHE', true);" $config_file
-	sed -i "69i define('WP_CACHE_KEY_SALT', 'wordpress_redis');" $config_file
+	sed -i "69i define('WP_CACHE_KEY_SALT', '${REDIS_CACHE_KEY_SALT}');" $config_file
 	wp redis enable --allow-root
 fi
 
